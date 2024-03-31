b93a3c1d02c2a3fa9312c5522fd9df18b0b711a9
bayram bartu <113308882+bartubayram@users.noreply.github.com>
2024-03-31 23:46:37 +0300

    Initial commit

---
+++ swiftUITest/ContentView.swift
@@ -0,0 +1,32 @@
+//
+//  ContentView.swift
+//  swiftUITest
+//
+//  Created by Bartu Kurnaz on 28.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, Swift!")
+            Text("Hello")
+                .foregroundStyle(.blue)
+
+        }
+        Text("Hello")
+
+        .padding()
+        Text("Hello")
+
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swiftUITest/swiftUITestApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUITestApp.swift
+//  swiftUITest
+//
+//  Created by Bartu Kurnaz on 28.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct swiftUITestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
